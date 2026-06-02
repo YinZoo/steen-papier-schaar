fdc2c90e8ee1555598f1e8a91c0aa59d294872d9
YinZoo <ramisowalha3@gmail.com>
2026-06-02 09:52:43 +0200

    Add files via upload

---
+++ Steen Papier Schaar/Javascript.js
@@ -0,0 +1,62 @@
+// Elementen ophalen
+const computerOutput = document.querySelector("#computer");
+const spelerOutput = document.querySelector("#speler");
+const resultaatOutput = document.querySelector("#resultaat");
+
+// Knoppen ophalen
+const steenButton = document.querySelector("#steen");
+const papierButton = document.querySelector("#papier");
+const schaarButton = document.querySelector("#schaar");
+
+// Mogelijke keuzes
+const keuzes = ["Steen", "Papier", "Schaar"];
+
+// Functie om een ronde te spelen
+function speelSpel(spelerKeuze) {
+
+    // Randomizer voor computer
+    let randomGetal = Math.floor(Math.random() * 3);
+    let computerKeuze = keuzes[randomGetal];
+
+    // Resultaat tonen
+    spelerOutput.innerHTML = spelerKeuze;
+    computerOutput.innerHTML = computerKeuze;
+
+    let resultaat = "";
+
+    // Winnaar bepalen
+    if (spelerKeuze === computerKeuze) {
+        resultaat = "Gelijkspel!";
+    }
+    else if (
+        (spelerKeuze === "Steen" && computerKeuze === "Schaar") || // || betekent "of"
+        (spelerKeuze === "Papier" && computerKeuze === "Steen") ||
+        (spelerKeuze === "Schaar" && computerKeuze === "Papier")
+    ) {
+        resultaat = "Speler wint!";
+    }
+    else {
+        resultaat = "Computer wint!";
+    }
+
+    // Resultaat tonen
+    resultaatOutput.innerHTML = resultaat;
+
+    // Console gebruiken
+    console.log("Speler:", spelerKeuze);
+    console.log("Computer:", computerKeuze);
+    console.log("Resultaat:", resultaat);
+}
+
+// Event handlers, dit zorgt dat er iets gebeurt als je op een knop klikt
+steenButton.addEventListener("click", function () {
+    speelSpel("Steen");
+});
+
+papierButton.addEventListener("click", function () {
+    speelSpel("Papier");
+});
+
+schaarButton.addEventListener("click", function () {
+    speelSpel("Schaar");
+});
