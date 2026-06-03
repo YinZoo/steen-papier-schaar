61e492a591f0e174046d20732c5d8371c57a8cda
YinZoo <ramisowalha3@gmail.com>
2026-06-03 11:57:47 +0200

    Winstreak toegevoegd
    Note:
    
    - Winstreak wordt NIET gereset op gelijkspel. Dit is met opzet gedaan.

--- Steen Papier Schaar V2/Javascript.js
+++ Steen Papier Schaar V2/Javascript.js
@@ -13,15 +13,21 @@ const keuzes = ["Steen", "Papier", "Schaar"];
 const spelerScoreOutput = document.querySelector("#spelerScore");
 const computerScoreOutput = document.querySelector("#computerScore");
 
+// Win streak elementen ophalen
+const winStreakOutput = document.querySelector("#winStreak");
+const hoogsteStreakOutput = document.querySelector("#hoogsteStreak");
+
 // score bijhouden
 let spelerScore = 0;
 let computerScore = 0;
+let winStreak = 0;
+let hoogsteStreak = 0;
 
 // Functie om een ronde te spelen
 function speelSpel(spelerKeuze) {
 
     // Randomizer voor computer
-    let randomGetal = Math.floor(Math.random() * 3);
+    let randomGetal = Math.floor(Math.random() * 3); // Math.random() geeft een getal tussen 0 en 1, dit vermenigvuldig ik met 3 zodat ik in totaal 3 keuzes heb (0, 1 en 2).
     let computerKeuze = keuzes[randomGetal];
 
     // Resultaat tonen
@@ -44,12 +50,21 @@ function speelSpel(spelerKeuze) {
         spelerScore++; // "++" verhoogt de score met 1
         spelerScoreOutput.innerHTML = spelerScore; //spelerScoreOutput.innerHTML werkt de score bij in de HTML
 
+        winStreak++;
+        winStreakOutput.innerHTML = winStreak;
+        if (winStreak > hoogsteStreak) { // Controleer of de huidige streak een nieuw record is
+            hoogsteStreak = winStreak;
+            hoogsteStreakOutput.innerHTML = hoogsteStreak;
+        }
     }
     else {
         resultaat = "Computer wint!";
 
         computerScore++;
         computerScoreOutput.innerHTML = computerScore;
+
+        winStreak = 0; // reset de win streak als de computer wint
+        winStreakOutput.innerHTML = winStreak;
     }
 
     // Resultaat tonen
